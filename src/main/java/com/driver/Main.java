dbd53fae766cf6d953e950f9b90b054aebe15bbb
Admin <purohitparthiv@gmail.com>
2023-12-13 00:06:21 +0530

    updated error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,27 @@
 package com.driver;
-public class Product
-    {
-      public int product(int x, int y) 
-      {
-        return x + y;
-      }
-      public int product(int x, int y, int z) 
-      {
-        return x + y + z;
-      }
-      public double product(double x, double y) 
-      {
-        return x + y;
-      }
-    }
 public class Main {
-  Product p = new Product();
-  System.out.println(p.product(1,2));
-  System.out.println(p.product(1,2,3));
-  System.out.pritnln(p.product(1.0,2.0);
+  public static void main(String[] args)
+  {
+    Product p = new Product();
+    System.out.println(p.product(1,2));
+    System.out.println(p.product(1,2,3));
+    System.out.println(p.product(1.0,2.0));
+  }
+
+
+  static class Product
+  {
+    public int product(int x, int y)
+    {
+      return x + y;
+    }
+    public int product(int x, int y, int z)
+    {
+      return x + y + z;
+    }
+    public double product(double x, double y)
+    {
+      return x + y;
+    }
+  }
 }
