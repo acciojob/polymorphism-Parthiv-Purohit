8e462f1d93354bbde4f2b0c41a597e6000da5f3b
Admin <purohitparthiv@gmail.com>
2023-12-14 23:26:48 +0530

    fix

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,9 @@ public class Main {
   public static void main(String[] args)
   {
     Product p = new Product();
-    System.out.println(p.product(1,2));
-    System.out.println(p.product(1,2,3));
-    System.out.println(p.product(1.0,2.0));
+   p.product(1,2);
+   p.product(1,2,3);
+   p.product(1.0,2.0);
   }
 
 
